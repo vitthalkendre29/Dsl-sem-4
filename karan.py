bcff37a294688c47b157feaf9e61498dc068341e
vitthalkendre29 <kendrevitthal225@gmail.com>
2024-03-18 23:51:00 +0530

    practice

--- karan.py
+++ karan.py
@@ -1,1 +1,17 @@
-print("kk")
+class calculator:
+    def add(self,x,y):
+        c=x+y
+        print(c)
+    def sub(self,x,y):
+        c=x-y
+        print(c)
+    def mul(self,x,y):
+        c=x*y
+        print(c)
+    def div(self,x,y):
+        c=x/y
+        print(c)
+
+
+vk=calculator()
+
