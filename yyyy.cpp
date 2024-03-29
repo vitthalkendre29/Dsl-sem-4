848f2b7b13bfeb44e3570d167cb3cfd222dc9713
vitthalkendre29 <kendrevitthal225@gmail.com>
2024-03-29 18:50:47 +0530

    new file added

---
+++ yyyy.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+enum Months{
+    sunday,monday,tuesday,friday
+};
+
+int main(){
+    /*
+    Months month=Months::friday;;
+
+    if(Months::friday){
+        cout<<"at no: "<<friday<<" is firday";
+    }else{
+        cout<<"vk";
+    }
+    */
+   /*
+   int vk=4;
+   float sk;
+   sk=vk;
+   cout<<sk;
+    return 0;
+    */
+}
